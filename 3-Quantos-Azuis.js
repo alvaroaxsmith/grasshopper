4d5994ab2a7ea6ea582cbc6db82f4c56c00e2b97
alvaroun <alvaromachadoferreira@hotmail.com>
2021-06-16 11:44:05 -0300

    Desenho de formas

---
+++ 3-Quantos-Azuis.js
@@ -0,0 +1,8 @@
+// Quantas caixas azuis serão desenhadas com esse código?
+
+drawBox(orange);
+drawBox(blue); // Apenas esta aqui.
+orange;
+newLine();
+drawBox(orange);
+blue;
---
+++ 7-Um-toque-de-aleatoriedade.js
@@ -0,0 +1,5 @@
+// Neste desafio foi apresentado a função pickRandom(), que deixa o código escolher a cor da caixa.
+
+drawBox(pickRandom(color));
+drawBox(pickRandom(color));
+//dois quadrados na mesma linha com cores escolhidas aleatóriamente.
