b254985a0b146a9df36fec452d9db343d2227682
alvaroun <alvaromachadoferreira@hotmail.com>
2021-06-17 23:32:53 -0300

    Arrays

---
+++ Arrays/4-Quadriculado.js
@@ -0,0 +1,18 @@
+// Neste desafio, foi usado indexação da array para acessar itens dentro de uma array. Indexação da array começa em 0. Por exemplo, myArray[0] vincula-se ao 1º item na array denominada myArray. Enquanto myArray[1] se vincula ao 2º item.
+
+var colors = [
+    'black',
+    'white',
+    ___
+  ];
+  drawBox(colors[0]); // str
+  drawBox(colors[1]);
+  drawBox(colors[0]);
+  newLine();
+  drawBox(colors[1]);
+  drawBox(colors[0]);
+  drawBox(colors[1]);
+  newLine();
+  drawBox(colors[0]);
+  drawBox(colors[1]);
+  drawBox(colors[0]);
